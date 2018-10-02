d385cf38b52fff8079dfba1fdd33633f0f5658d3
Adithya Kalaparti <adithya.kalaparthy@gmail.com>
2018-10-02 18:45:46 +0200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,9 +1,29 @@
+/*document.getElementById('start').addEventListener("keypress",disappear());*/
+window.addEventListener("keypress", event => {
+    if (event.keyCode == 13 && $('#sub').attr('class')=="container h-100 d-flex justify-content-center align-items-center") {
+      $("#start").click();
+    }
+    else if(event.keyCode==13 && $('#ques1').attr('class')=="container invisible" && $('#ques2').attr('class')=="container invisible"){
+      $("#submitb").click();
+    }
+    else{
+      alert("Please answer 2 questions");
+    }
+  });
+
 function disappear() {
+  jQuery(function($){
+    var n = $('#ques1').offset().top - 100; //height to be scrolled
+    $('html, body').animate({ scrollTop: n }, 600);
+  });
    document.getElementById('sub').className = "d-none";
    document.getElementById('ques1').className = "container";
-   document.getElementById('ques2').className = "container";
-   document.getElementById('submit').className = "container submit col-lg-12";
-   }
+   document.getElementById('ques2').className = "container invisible";
+   document.getElementById('submit').className = "container submit col-lg-12 invisible";
+   };
+
+
+
 function disappear2() {
    document.getElementById('sub').className = "d-none";
    document.getElementById('ques1').className = "container d-none";
@@ -11,62 +31,55 @@ function disappear2() {
    document.getElementById('submit').className = "container submit col-lg-12 d-none";
    document.getElementById('image').className = "text-center h-100";
    }
-  
-$(function() {            
-   var documentEl = $(document),
-   fadeElem = $('#ques2'); 
-   fadeElem2 = $('#ques1'); 
-   fadeElem3 = $('#submit');
-   var lastScrollTop = 0;
-   $(window).scroll(function(event){
-      var st = $(this).scrollTop();
-      if (st > lastScrollTop){
-          fadeElem.each(function() {
-            var $this = $(this),
-            elemOffsetTop = $this.offset().top;
-            $this.css('opacity', 1);
-            }); 
-          fadeElem2.each(function() {
-             var $this = $(this),
-             elemOffsetTop = $this.offset().top;
-             $this.css('opacity', 0.4);
-             }); 
-          fadeElem3.each(function() {
-             var $this = $(this),
-             elemOffsetTop = $this.offset().top;
-             $this.css('opacity', 1);
-             });   
-       }  
-      
-      else {
-           fadeElem.each(function() {
-              var $this = $(this),
-              elemOffsetTop = $this.offset().top;
-              $this.css('opacity', 0.4);
-              }); 
 
-           fadeElem2.each(function() {
-              var $this = $(this),
-              elemOffsetTop = $this.offset().top;
-              $this.css('opacity', 1);
-              }); 
-           fadeElem3.each(function() {
-              var $this = $(this),
-              elemOffsetTop = $this.offset().top;
-              $this.css('opacity', 0.4);
-              });
-           }
-      lastScrollTop = st;
-      }); 
- });
+$("#cat").click(function(event){
 
-$(".gap").click(function(event){
-
-  var n = '+250px'; //height to be scrolled
+  var n = $('#ques2').offset().top - 100; //height to be scrolled
   $('html, body').animate({ scrollTop: n }, 600);
-});
+  document.getElementById('ques1').className = "container invisible"
+  document.getElementById('ques2').className = "container";
+  document.getElementById('question2').innerHTML = "2. What do you wanna name your cat?";
+  document.getElementById('2option1').innerHTML = "oliver";
+  document.getElementById('2option2').innerHTML = "willie";
+  document.getElementById('2option3').innerHTML = "Tom";
+    });
+
+$("#dog").click(function(event){
+
+  var n = $('#ques2').offset().top - 100; //height to be scrolled
+  $('html, body').animate({ scrollTop: n }, 600);
+  document.getElementById('ques1').className = "container invisible"
+  document.getElementById('ques2').className = "container";
+  document.getElementById('question2').innerHTML = "2. Which breed of dog do you want?";
+  document.getElementById('2option1').innerHTML = "Golden-Retriever";
+  document.getElementById('2option2').innerHTML = "Pug";
+  document.getElementById('2option3').innerHTML = "Labrador";
+    });
+
+$("#panda").click(function(event){
+
+  var n = $('#ques2').offset().top - 100; //height to be scrolled
+  $('html, body').animate({ scrollTop: n }, 600);
+  document.getElementById('ques1').className = "container invisible"
+  document.getElementById('ques2').className = "container";
+  document.getElementById('question2').innerHTML = "2. What color of panda do you like?";
+  document.getElementById('2option1').innerHTML = "White";
+  document.getElementById('2option2').innerHTML = "Black and White";
+  document.getElementById('2option3').innerHTML = "Chocolate";
+    });
+
 
 $(".crazy").click(function(event){
-  var n = '+750px'; //height to be scrolled
+  var n = $('#submit').offset().top - 100; //height to be scrolled
   $('html, body').animate({ scrollTop: n }, 700);
+  document.getElementById('ques2').className = "container invisible"
+  document.getElementById('submit').className = "container submit col-lg-12"
+});
+
+$(".changes").click(function(event){
+  var n = $('#ques1').offset().top - 100; //height to be scrolled
+  $('html, body').animate({ scrollTop: n }, 600);
+  document.getElementById('ques1').className = "container"
+  document.getElementById('ques2').className = "container invisible";
+  document.getElementById('submit').className = "container submit col-lg-12 invisible"
 });
