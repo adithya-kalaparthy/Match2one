f0b9728cb696336b58397bf3437c304c815d8b32
Adithya Kalaparti <adithya.kalaparthy@gmail.com>
2018-10-15 15:16:15 +0200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,4 +1,5 @@
-/*document.getElementById('start').addEventListener("keypress",disappear());*/
+
+/*This function checks if the enter key is pressed and directs the operations appropriately*/
 window.addEventListener("keypress", event => {
     if (event.keyCode == 13 && $('#sub').attr('class')=="container h-100 d-flex col-12") {
       $("#start").click();
@@ -10,7 +11,7 @@ window.addEventListener("keypress", event => {
       location.reload();
     }
   });
-
+/*The starting function which removes the start button from screen and displays the 1st question*/
 function disappear() {
   jQuery(function($){
     var h = $('#ques1').offset().top - 150; //height to be scrolled
@@ -24,7 +25,7 @@ function disappear() {
    };
 
 
-
+/*it is the end function which removes the questions from the screen and displays the end result*/
 function disappear2() {
    /*document.getElementById('sub').className = "d-none";*/
    if ($('input[name=animal]:checked', '#quiz').val() && $('input[name=op1]:checked', '#quiz2').val() ) {
@@ -42,13 +43,14 @@ function disappear2() {
    }
    
    }
-
+/*This function happens when user selects any option for the 1st question*/
 $("#quiz input").on('change',function(){
   
-  document.getElementById('ques2').className = "container q";
-  document.getElementById('ques1').className="container invisible"
+  document.getElementById('ques2').className = "container q"; /*display the 2nd question*/
+  document.getElementById('ques1').className="container invisible" /*make the 1st question invisible*/
   var n = $('#ques2').offset().top; //height to be scrolled
-  $('html, body').animate({ scrollTop: n });
+  $('html, body').animate({ scrollTop: n }); /*animation of scroll function. Delay hass been removed so it doesnot clash with autoscroll*/
+  /*these if conditions check which option has been selected and changes the 2nd question and options appropriately */
   if( $('input[name=animal]:checked', '#quiz').val() == "cat"){
       document.getElementById("question2").innerHTML = "2.What do you wanna name your cat?";
       document.getElementById("2option1").innerHTML="oliver";
@@ -79,13 +81,16 @@ $("#quiz input").on('change',function(){
 
  });
 
+/*auto scroll function*/
 $( window).scroll(function(){
-    var n = $(window).scrollTop();
-    if ($('#image').attr('class')=="text-center h-100 q") {
+    var n = $(window).scrollTop(); /*position of scroll*/
+    if ($('#image').attr('class')=="text-center h-100 q") { /*checking if the image is active on the screen*/
       document.getElementById("ques1").className = "d-none";
       document.getElementById("ques2").className = "d-none"
       document.getElementById("submit").className = "d-none";
     }
+    /*the following else if works only if 1st question is answered but the 2nd question is yet to be done*/
+    /* this is necessary because if the user decides to change their answer after 1st question is answered*/
     else if ( $('input[name=animal]:checked', '#quiz').val() && !$('input[name=op1]:checked', '#quiz2').val() ) {
       if (n < $('#ques1').offset().top-200){
         document.getElementById('ques1').className="container q";
@@ -98,6 +103,7 @@ $( window).scroll(function(){
         console.log(n);
       }
     }
+    /*the following else if works if both the questions are answered and if user decides to change any of their answers*/
     else if ( $('input[name=animal]:checked', '#quiz').val() && $('input[name=op1]:checked', '#quiz2').val()) {
       if (n < $('#ques1').offset().top-50) {
         document.getElementById('ques1').className="container q"
@@ -118,11 +124,10 @@ $( window).scroll(function(){
 });
 
 
-
+/*this function is activated by answering the second question*/
 $(".crazy").click(function(event){
-  document.getElementById('submit').className="container submit col-lg-12 q";
-  document.getElementById('ques2').className="container invisible";
+  document.getElementById('submit').className="container submit col-lg-12 q"; /*display the submit button*/
+  document.getElementById('ques2').className="container invisible";/*make the second question invisible*/
   var n = $('#submit').offset().top; //height to be scrolled
   $('html, body').animate({ scrollTop: n });
-  /*invisible*/
 });
